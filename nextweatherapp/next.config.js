2db5f3c26cf762636b3868455ec73ad9503b43da
Justin Oeters <justin.oeters@gmail.com>
2022-03-08 20:33:27 -0500

    connected city search to openweather api

--- nextweatherapp/next.config.js
+++ nextweatherapp/next.config.js
@@ -1,6 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+require("dotenv").config();
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+};
---
+++ nextweatherapp/pages/location/[city].js
@@ -0,0 +1,60 @@
+import React from "react";
+import cities from "../../lib/city.list.json";
+
+export async function getServerSideProps(context) {
+  const city = getCity(context.params.city);
+
+  if (!city) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const res = await fetch(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${city.coord.lat}&lon=${city.coord.lon}&appid=${process.env.API_KEY}&units=imperial&exclude=minutely`
+  );
+
+  const data = await res.json();
+
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const slug = context.params.city;
+
+  return {
+    props: { slug: slug, data: data },
+  };
+}
+
+//helper function to get city ID
+
+const getCity = (param) => {
+  const cityParam = param.trim();
+  // get city ID
+  const splitCity = cityParam.split("-");
+  const id = splitCity[splitCity.length - 1];
+
+  if (!id) {
+    return null;
+  }
+
+  const city = cities.find((city) => city.id.toString() == id);
+
+  if (city) {
+    return city;
+  } else {
+    return null;
+  }
+};
+
+export default function City({ slug, data }) {
+  return (
+    <div>
+      <h1>City Page</h1>
+      <h2>{slug}</h2>
+    </div>
+  );
+}
