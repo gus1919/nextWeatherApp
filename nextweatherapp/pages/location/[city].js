fa55932dda804202641463c98d0a65d2485fff06
Justin Oeters <justin.oeters@gmail.com>
2022-03-12 08:14:57 -0500

    edited props and imported components

--- nextweatherapp/pages/location/[city].js
+++ nextweatherapp/pages/location/[city].js
@@ -1,5 +1,8 @@
 import React from "react";
 import cities from "../../lib/city.list.json";
+import Head from "next/head";
+import TodaysWeather from "../../components/TodaysWeather";
+import moment from "moment-timezone";
 
 export async function getServerSideProps(context) {
   const city = getCity(context.params.city);
@@ -22,10 +25,14 @@ export async function getServerSideProps(context) {
     };
   }
 
-  const slug = context.params.city;
-
   return {
-    props: { slug: slug, data: data },
+    props: {
+      city: city,
+      timezone: data.timezone,
+      currentWeather: data.current,
+      dailyWeather: data.daily,
+      hourlyWeather: getHourlyWeather(data.hourly, data.timezone),
+    },
   };
 }
 
@@ -50,11 +57,38 @@ const getCity = (param) => {
   }
 };
 
-export default function City({ slug, data }) {
+// another helper function to get hourly data for a limited time
+const getHourlyWeather = (hourlyData, timezone) => {
+  const endOfDay = moment().tz(timezone).endOf("day").valueOf();
+  const eodTimeStamp = Math.floor(endOfDay / 1000);
+
+  const todaysData = hourlyData.filter((data) => data.dt < eodTimeStamp);
+
+  return todaysData;
+};
+
+export default function City({
+  hourlyWeather,
+  currentWeather,
+  dailyWeather,
+  city,
+  timezone,
+}) {
+  // console.log(hourlyWeather);
   return (
     <div>
-      <h1>City Page</h1>
-      <h2>{slug}</h2>
+      <Head>
+        <title>{city.name} Weather</title>
+      </Head>
+      <div className="page-wrapper">
+        <div className="container">
+          <TodaysWeather
+            city={city}
+            weather={dailyWeather[0]}
+            timezone={timezone}
+          />
+        </div>
+      </div>
     </div>
   );
 }
