89d40179b10d20192fe0235889fe45553819b5b6
Justin Oeters <justin.oeters@gmail.com>
2022-03-12 18:04:04 -0500

    added for hardcoded locations in ohio

--- nextweatherapp/components/FamousPlaces.js
+++ nextweatherapp/components/FamousPlaces.js
@@ -1,5 +1,59 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+//import specific images
+import CincinnatiImage from "../public/images/Cincinnati_OH.jpg";
+import WilmingtonImage from "../public/images/Wilmington_OH.jpg";
+import ColumbusImage from "../public/images/Columbus_OH.jpg";
+import ClevelandImage from "../public/images/Cleveland_OH.jpg";
+
+const places = [
+  {
+    name: "Cincinnati",
+    image: CincinnatiImage,
+    url: "/location/cincinnati-4508722",
+  },
+  {
+    name: "Wilmington",
+    image: WilmingtonImage,
+    url: "/location/wilmington-4528463",
+  },
+  {
+    name: "Columbus",
+    image: ColumbusImage,
+    url: "/location/columbus-4509177",
+  },
+  {
+    name: "Cleveland",
+    image: ClevelandImage,
+    url: "/location/cleveland-5150529",
+  },
+];
 
 export default function FamousPlaces() {
-  return <div>FamousPlaces</div>;
+  return (
+    <div className="places">
+      <div className="places__row">
+        {places.length > 0 &&
+          places.map((place, index) => (
+            <div className="places__box" key={index}>
+              <Link href={place.url}>
+                <a>
+                  <div className="places__image-wrapper">
+                    <Image
+                      src={place.image}
+                      alt={`${place.name}`}
+                      layout="fill"
+                      objectFit="cover"
+                    />
+                  </div>
+                  <span>{place.name}</span>
+                </a>
+              </Link>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 }
